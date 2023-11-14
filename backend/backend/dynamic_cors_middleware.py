d5037840d188207478aa7b4db6edddfcc436853b
Akhil-Patil-Bagili <akhilpatilbagili@gmail.com>
2023-11-13 17:10:18 -0800

    changed new fil

--- backend/backend/dynamic_cors_middleware.py
+++ backend/backend/dynamic_cors_middleware.py
@@ -6,11 +6,17 @@ class DynamicCorsMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-
         origin = request.META.get('HTTP_ORIGIN', '')
 
+        # Check if the origin is allowed
         if ".netlify.app" in origin or origin in ["http://localhost:3000"]:
             response["Access-Control-Allow-Origin"] = origin
             response["Vary"] = "Origin"
+
+            # Handle OPTIONS request for preflight
+            if request.method == "OPTIONS":
+                response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+                response["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+                response["Access-Control-Max-Age"] = "86400"  # 24 hours
 
         return response
