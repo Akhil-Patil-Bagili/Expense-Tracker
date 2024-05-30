bbe93f8dd28b5b60b5eb1a9cabf15f006e36b38c
Akhil Patil Bagili <akhilpatilbagili@gmail.com>
2024-05-29 19:42:32 -0700

    Update Home.js

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,7 +4,8 @@ import "../Styles/App.css";
 
 const Home = () => (
   <div className="form-box-home">
-    <h1>Less stress when <br /> tracking finances</h1>
+    {/* <h1>Less stress when <br /> tracking finances</h1> */}
+    <div className="static-text">Less stress when <br /> tracking finances</div>
     <RotatingText />
   </div>
 );
